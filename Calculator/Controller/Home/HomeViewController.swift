dad1d14c222cde753365cf3ee7296c2f8b3747d5
Igor Dzichkovskii <Igordzich@gmail.com>
2020-08-12 00:01:09 +0200

    Making the project structure, adding home view and controller.

---
+++ Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Calculator
+//
+//  Created by Vitcord23 on 11/08/2020.
+//  Copyright © 2020 IgorDzichkovskii. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
