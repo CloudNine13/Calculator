e051550834428a8f8a69d8db197ffc66ca3a947f
Igor Dzichkovskii <Igordzich@gmail.com>
2020-08-12 08:46:47 +0200

    Storyboard now has different views and stacks. Preparation of its files to code.

--- Calculator/Controller/Home/HomeViewController.swift
+++ Calculator/Controller/Home/HomeViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    // MARK: - Outlets
+    
+    // Result
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Numbers
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var numberDecimal: UIButton!
+    
+    // Operators
+    @IBOutlet weak var operatorAC: UIButton!
+    @IBOutlet weak var operatorPlusMinus: UIButton!
+    @IBOutlet weak var operatorPercent: UIButton!
+    @IBOutlet weak var operatorResult: UIButton!
+    @IBOutlet weak var operatorPlus: UIButton!
+    @IBOutlet weak var operatorMinus: UIButton!
+    @IBOutlet weak var operatorMultiplication: UIButton!
+    @IBOutlet weak var operatorDivision: UIButton!
+    
+    
     // MARK: - Initialization
     
     init() {
@@ -27,4 +56,37 @@ final class HomeViewController: UIViewController {
         
         
     }
+    
+    // MARK: - Button Actions
+    
+    @IBAction func operatorACAction(_ sender: Any) {
+    }
+    
+    @IBAction func operatorPlusMinusAction(_ sender: Any) {
+    }
+
+    @IBAction func operatorPercentAction(_ sender: Any) {
+    }
+    
+    @IBAction func operatorResultAction(_ sender: Any) {
+    }
+    
+    @IBAction func operatorPlusAction(_ sender: Any) {
+    }
+    
+    @IBAction func operatorMinusAcion(_ sender: Any) {
+    }
+    
+    @IBAction func operatorMultiplicationAction(_ sender: Any) {
+    }
+    
+    @IBAction func operatorDivisionAction(_ sender: Any) {
+    }
+    
+    @IBAction func NumberDecimalAction(_ sender: Any) {
+    }
+ 
+    @IBAction func numberAction(_ sender: UIButton) {
+    }
+    
 }
---
+++ Calculator/Utils/UIButtonExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIButtonExtension.swift
+//  Calculator
+//
+//  Created by Vitcord23 on 12/08/2020.
+//  Copyright © 2020 IgorDzichkovskii. All rights reserved.
+//
+
+import UIKit
+
+class UIButtonExtension: NSObject {
+
+}
